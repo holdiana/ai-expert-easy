fa5a0c539bea7a4383a0abaa92a5b78b97e00d39
Diana Holoborodko <diana.holoborodko@gmail.com>
2023-11-07 16:52:02 +0200

    test commit for git

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -7,6 +7,7 @@ const Home:React.FC = () => {
   return (
     <S.Container >
       <Typography variant="h4">Home Page 111</Typography>
+      <Typography variant='h1'>Test git</Typography>
       {/* Render your table here using the data */}
     </S.Container>
   );
